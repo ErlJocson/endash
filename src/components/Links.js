ee9739880aa511a28ba8d1aa705b7470dbe13d6e
erljocson <erl.jocson@teamhgs.com>
2024-07-16 23:27:14 +0800

    new cards

--- src/components/Links.js
+++ src/components/Links.js
@@ -1,4 +1,121 @@
+// export const links = [
+//   {
+//     title: "Digital Transformation",
+//     icon: "/icons/digital transformation.png",
+//     link: "/videos/v1.mp4",
+//     website: "http://www.google.com/search?q=digital+transformation",
+//   },
+//   {
+//     title: "Automation",
+//     icon: "/icons/automate.png",
+//     link: "/videos/v1.mp4",
+//     website: "http://www.google.com/search?q=automation",
+//   },
+//   {
+//     title: "Interaction Analytics",
+//     icon: "/icons/interaction analytics.png",
+//     link: "/videos/v1.mp4",
+//     website: "http://www.google.com/search?q=interaction+analytics",
+//   },
+//   {
+//     title: "Translation Bot",
+//     icon: "/icons/translation bot.png",
+//     link: "/videos/v1.mp4",
+//     website: "http://www.google.com/search?q=translation+bot",
+//   },
+//   {
+//     title: "Vision AI",
+//     icon: "/icons/vision ai.png",
+//     link: "/videos/v1.mp4",
+//     website: "http://www.google.com/search?q=vision+ai",
+//   },
+//   {
+//     title: "Gamification",
+//     icon: "/icons/gamification.png",
+//     link: "/videos/v1.mp4",
+//     website: "http://www.google.com/search?q=gamification",
+//   },
+//   {
+//     title: "Data Analytics",
+//     icon: "/icons/data analytics.png",
+//     link: "/videos/v1.mp4",
+//     website: "http://www.google.com/search?q=data+analytics",
+//   },
+// ];
+
 export const links = [
+  {
+    title: "Automation",
+    icon: "/icons/digital transformation.png",
+    link: "/videos/v1.mp4",
+    website: "http://www.google.com/search?q=digital+transformation",
+  },
+  {
+    title: "Data Analytics",
+    icon: "/icons/digital transformation.png",
+    link: "/videos/v1.mp4",
+    website: "http://www.google.com/search?q=digital+transformation",
+  },
+  {
+    title: "Interaction Analytics",
+    icon: "/icons/digital transformation.png",
+    link: "/videos/v1.mp4",
+    website: "http://www.google.com/search?q=digital+transformation",
+  },
+  {
+    title: "Data Visualization",
+    icon: "/icons/digital transformation.png",
+    link: "/videos/v1.mp4",
+    website: "http://www.google.com/search?q=digital+transformation",
+  },
+  {
+    title: "Application Development",
+    icon: "/icons/digital transformation.png",
+    link: "/videos/v1.mp4",
+    website: "http://www.google.com/search?q=digital+transformation",
+  },
+  {
+    title: "Gamification",
+    icon: "/icons/digital transformation.png",
+    link: "/videos/v1.mp4",
+    website: "http://www.google.com/search?q=digital+transformation",
+  },
+  {
+    title: "Visual Intelligence",
+    icon: "/icons/digital transformation.png",
+    link: "/videos/v1.mp4",
+    website: "http://www.google.com/search?q=digital+transformation",
+  },
+  {
+    title: "Translation Bot",
+    icon: "/icons/digital transformation.png",
+    link: "/videos/v1.mp4",
+    website: "http://www.google.com/search?q=digital+transformation",
+  },
+  {
+    title: "Chat Bot",
+    icon: "/icons/digital transformation.png",
+    link: "/videos/v1.mp4",
+    website: "http://www.google.com/search?q=digital+transformation",
+  },
+  {
+    title: "Quality Module",
+    icon: "/icons/digital transformation.png",
+    link: "/videos/v1.mp4",
+    website: "http://www.google.com/search?q=digital+transformation",
+  },
+  {
+    title: "Decision Trees",
+    icon: "/icons/digital transformation.png",
+    link: "/videos/v1.mp4",
+    website: "http://www.google.com/search?q=digital+transformation",
+  },
+  {
+    title: "Social Media",
+    icon: "/icons/digital transformation.png",
+    link: "/videos/v1.mp4",
+    website: "http://www.google.com/search?q=digital+transformation",
+  },
   {
     title: "Digital Transformation",
     icon: "/icons/digital transformation.png",
@@ -6,39 +123,9 @@ export const links = [
     website: "http://www.google.com/search?q=digital+transformation",
   },
   {
-    title: "Automation",
-    icon: "/icons/automate.png",
+    title: "Agent X",
+    icon: "/icons/digital transformation.png",
     link: "/videos/v1.mp4",
-    website: "http://www.google.com/search?q=automation",
-  },
-  {
-    title: "Interaction Analytics",
-    icon: "/icons/interaction analytics.png",
-    link: "/videos/v1.mp4",
-    website: "http://www.google.com/search?q=interaction+analytics",
-  },
-  {
-    title: "Translation Bot",
-    icon: "/icons/translation bot.png",
-    link: "/videos/v1.mp4",
-    website: "http://www.google.com/search?q=translation+bot",
-  },
-  {
-    title: "Vision AI",
-    icon: "/icons/vision ai.png",
-    link: "/videos/v1.mp4",
-    website: "http://www.google.com/search?q=vision+ai",
-  },
-  {
-    title: "Gamification",
-    icon: "/icons/gamification.png",
-    link: "/videos/v1.mp4",
-    website: "http://www.google.com/search?q=gamification",
-  },
-  {
-    title: "Data Analytics",
-    icon: "/icons/data analytics.png",
-    link: "/videos/v1.mp4",
-    website: "http://www.google.com/search?q=data+analytics",
+    website: "http://www.google.com/search?q=digital+transformation",
   },
 ];
